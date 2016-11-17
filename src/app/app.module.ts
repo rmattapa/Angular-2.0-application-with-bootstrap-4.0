c623e8ad23b2f4c9a51b233472492efd8b902274
Rajesh <rajeshprince186@gmail.com>
2016-11-17 09:57:32 -0600

    Pushing entire application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {StocksComponent} from './stocks.component';
+import {HighlightDirective} from './highlight.directive';
+import {StockService} from './stock.service';
+import {routing} from './app.routing';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
+import { StockDirectiveDirective } from './stock-directive.directive';
+import { DateFormatterPipe } from './date-formatter.pipe';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocksComponent,
+    HighlightDirective,
+    MutualfundsComponent,
+    StockDirectiveDirective,
+    DateFormatterPipe,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
